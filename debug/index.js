9bac798bf82cf4de4c9582f001dfa59326d0f36f
Alex Kempf <ackempf@eagles.usi.edu>
2017-01-19 11:54:00 -0600

    (fix) Fixed no return on Promise if RSS was unable to find data at given url

--- debug/index.js
+++ debug/index.js
@@ -7,7 +7,7 @@ var api = require('../');
 function resolve(d) {
     console.log(d);
 }
-api.rs.player.events('sync').then(console.log).catch(console.error);
+api.rs.player.events('enlighten').then(console.log).catch(console.error);
 //api.rs.distraction.viswax.getCurrent().then(console.log).catch(console.error);
 
 //api.rs.boss.araxxor.getRotation(new Date()).then(console.log).catch(console.error);
